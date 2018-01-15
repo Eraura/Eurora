d08279f768809808ab9a9fa939dba40d9f6f4a26
Eraura <35397951+Eraura@users.noreply.github.com>
2018-01-15 15:22:47 +0100

    Update Process.h

--- Core/Process.h
+++ Core/Process.h
@@ -1,3 +1,10 @@
+/*
+Copyright 2018 Eurora Technologies, all rights reserved
+Author: Samuel Alonso
+Contact: somethingghanging@gmail.com
+Date: 15/01/2018(dd/mm/yyyy)
+Version: 1
+*/
 #pragma once
 
 #include <Eurora\Types.h>
